784d149554acd9db9c4fa7dd7c96b372810769a7
azerckid <azerckid@gmail.com>
2025-05-01 15:21:20 +0900

    #2.2 movies service 1/2

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Param, Post, Delete, Patch, Body, Query } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) { }
+
+    @Get()
+    getAll() {
+        return this.moviesService.getAll();
+    }
+
+    @Get('search')
+    search(@Query('year') searchingYear: string) {
+        return this.moviesService.search(searchingYear);
+    }
+
+    @Get('/:id')
+    getOne(@Param('id') movieId: string) {
+        return this.moviesService.getOne(movieId);
+    }
+
+    @Post()
+    create(@Body() movieData) {
+        return this.moviesService.create(movieData);
+    }
+
+    @Delete('/:id')
+    remove(@Param('id') movieId: string) {
+        return this.moviesService.deleteOne(movieId);
+    }
+
+    @Patch('/:id')
+    patch(@Param('id') movieId: string, @Body() updateData) {
+        return this.moviesService.update(movieId, updateData);
+    }
+
+}
